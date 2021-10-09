e672a3793e01b563cdbe0918a8a0e4b38f5e8070
Ananya-Ananda <86326693+Ananya-Ananda@users.noreply.github.com>
2021-10-09 17:16:15 -0400

    second push

--- unitConverter.js
+++ unitConverter.js
@@ -1,5 +1,43 @@
-function convertCupToTb(cups) {
-    let tb = 16 * cups;
-    document.write(tb);
+function convertCupToTbsp(cups) {
+    let tbsp = 16 * cups;
+    document.getElementById("cupToTb").innerHTML = tbsp; // write to HTML from javascript
+}
+
+/*
+Conversions:
+    - ounces to cups
+    - ounces to tablespoons
+    - tablespoons to teaspoons
+    - ounces to teaspoons
+*/
+
+//one tbsp = 3 teaspoons
+function convertTbspToTsp(tbsp){
+    let tsp = 3 * tbsp;
+    document.getElementById("tbToTsp").innerHTML = tsp;
+}
+
+//one cup = 48 teaspoons
+function convertCupsToTsp(cups){
+    let tsp = 48 * cups;
+    document.getElementById("cupToTsp").innerHTML = tsp;
+}
+//ounces to cups - 1 oz = 0.125 cups
+function convertOzToCups(oz){
+    let cups = 0.125 * oz;
+    document.getElementById("ozToCups").innerHTML = cups;
+}
+
+//ounces to tbsp - 1 ounc = 2 tbsp
+function convertOzToTbsp(oz){
+    let tbsp = 2 * oz;
+    document.getElementById("ozToTbsp").innerHTML = tbsp;
+}
+
+//ounces to tsp - 1 ounce = 6 teaspoons
+function convertOzToTsp(oz){
+    let tsp = 6 * oz;
+    document.getElementById("ozToTsp").innerHTML = tsp;
+
 }
 
